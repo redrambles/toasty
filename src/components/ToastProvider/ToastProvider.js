148ca6042fda671eb0fffae18c99de227280a756
Ann Cascarano <4411121+redrambles@users.noreply.github.com>
2023-09-07 08:11:48 -0400

    Add event listener to provider to dismiss all toasts

--- src/components/ToastProvider/ToastProvider.js
+++ src/components/ToastProvider/ToastProvider.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export const ToastContext = React.createContext();
 
 function ToastProvider({ children }) {
   const [toasts, setToasts] = useState([]); // [{ id: Date.now(), variant: "notice", message: "Hello world!" }
+
+  useEffect(() => {
+    const dismissAll = (e) => {
+      if (e.key === "Escape") {
+        setToasts([]);
+      }
+    };
+    window.addEventListener("keyup", dismissAll);
+
+    return () => window.removeEventListener("keyup", dismissAll);
+  }, []);
 
   const addToast = (variant, message) => {
     const newToast = { id: Date.now(), variant, message };
